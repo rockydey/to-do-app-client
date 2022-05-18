8e8d40bf85131f0589a377bf8e1c31474e978fc4
rockydey <coderrocky25@gmail.com>
2022-05-19 02:51:06 +0600

    home page complete

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
 import MyTask from './Pages/MyTask/MyTask';
+import Navbar from './Pages/Shared/Navbar';
 import SignIn from './Pages/SignIn/SignIn';
 import SignUp from './Pages/SignIn/SignUp';
+import ToDo from './Pages/ToDo/ToDo';
 
 function App() {
   return (
-    <div>
+    <div className='w-10/12 mx-auto'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/mytask' element={<MyTask></MyTask>}></Route>
+        <Route path='/todo' element={<ToDo></ToDo>}></Route>
         <Route path='/signin' element={<SignIn></SignIn>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <header className='h-[80vh] flex items-center justify-center'>
+            <div className='text-center'>
+                <h1 className='lg:text-5xl text-3xl font-bold text-green-500'>Welcome to ToDo Apps!</h1>
+                <Link to='/todo' className='btn btn-success btn-outline uppercase mt-5'>Add a Task</Link>
+            </div>
+        </header>
     );
 };
 
